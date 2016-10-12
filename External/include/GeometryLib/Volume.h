2958c5e3513279cf5e6195fb517b30970ee38a49
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2016-10-12 16:56:29 +0300

    Add new volume - capsule

--- External/include/GeometryLib/Volume.h
+++ External/include/GeometryLib/Volume.h
@@ -3,34 +3,41 @@
 
 #include "Utility.h"
 
-class Volume
-{
-public:
-	virtual bool IncludesPoint(const VM::vec4& point) const = 0;
-	virtual bool IntersectsWithCube(
-		const VM::vec4& minPoint,
-		const VM::vec4& maxPoint) const = 0;
+class Volume {
+	public:
+		virtual bool IncludesPoint(const VM::vec4& point) const = 0;
+		virtual bool IntersectsWithCube(
+			const VM::vec4& minPoint,
+			const VM::vec4& maxPoint) const = 0;
 };
 
 class Tube: public Volume {
-private:
-	VM::vec4 Begin;
-    VM::vec4 End;
-    VM::vec4 Direction;
-    void UpdateDirection();
-public:
-	Tube();
-	Tube(const VM::vec4& begin, const VM::vec4& end, const float radius);
+	protected:
+		VM::vec4 Begin;
+		VM::vec4 End;
+		VM::vec4 Direction;
+		void UpdateDirection();
+	public:
+		Tube();
+		Tube(const VM::vec4& begin, const VM::vec4& end, const float radius);
 
-    void SetBegin(const VM::vec4& point);
-    void SetEnd(const VM::vec4& point);
+		void SetBegin(const VM::vec4& point);
+		void SetEnd(const VM::vec4& point);
 
-	virtual bool IncludesPoint(const VM::vec4& point) const;
-    virtual bool IntersectsWithCube(
-		const VM::vec4& minPoint,
-		const VM::vec4& maxPoint) const;
+		bool IncludesPoint(const VM::vec4& point) const;
+		bool IntersectsWithCube(
+			const VM::vec4& minPoint,
+			const VM::vec4& maxPoint) const;
 
-    float Radius;
+		float Radius;
 };
 
+class Capsule: public Tube {
+public:
+		Capsule();
+		Capsule(const VM::vec4& begin, const VM::vec4& end, const float radius):
+		Tube(begin, end, radius) {};
+
+		bool IncludesPoint(const VM::vec4& point) const;
+};
 #endif // VOLUME_H
--- GeometryLib/src/Volume.cpp
+++ GeometryLib/src/Volume.cpp
@@ -9,7 +9,7 @@ Tube::Tube() {
     UpdateDirection();
 }
 
-Tube::Tube(const VM::vec4& begin, const VM::vec4& end, const float radius) {
+Tube::Tube(const vec4& begin, const vec4& end, const float radius) {
     Begin = begin;
     End = end;
     Radius = radius;
@@ -20,24 +20,24 @@ void Tube::UpdateDirection() {
     Direction = normalize(End - Begin);
 }
 
-void Tube::SetBegin(const VM::vec4& point) {
+void Tube::SetBegin(const vec4& point) {
     Begin = point;
     UpdateDirection();
 }
 
-void Tube::SetEnd(const VM::vec4& point) {
+void Tube::SetEnd(const vec4& point) {
     End = point;
     UpdateDirection();
 }
 
-bool Tube::IncludesPoint(const VM::vec4& point) const {
+bool Tube::IncludesPoint(const vec4& point) const {
 	vec4 dirToPoint = point - Begin;
     return length(cross(Direction, dirToPoint)) / length(dirToPoint) <= Radius;
 }
 
 bool Tube::IntersectsWithCube(
-		const VM::vec4& minPoint,
-		const VM::vec4& maxPoint) const
+		const vec4& minPoint,
+		const vec4& maxPoint) const
 {
     return IncludesPoint(minPoint)
 		|| IncludesPoint(vec4(minPoint.x, minPoint.y, maxPoint.z, 1))
@@ -47,4 +47,12 @@ bool Tube::IntersectsWithCube(
 		|| IncludesPoint(vec4(maxPoint.x, minPoint.y, maxPoint.z, 1))
 		|| IncludesPoint(vec4(maxPoint.x, maxPoint.y, minPoint.z, 1))
 		|| IncludesPoint(maxPoint);
+}
+
+bool Capsule::IncludesPoint(const vec4& point) const {
+    return (Tube::IncludesPoint(point)
+		&& dot(Direction, point - Begin) > 0
+		&& dot(Direction, point - End) < 0)
+		|| length(point - Begin) < Radius
+		|| length(point - End) < Radius;
 }
