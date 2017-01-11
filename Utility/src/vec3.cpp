280e72db3c7cee2c1be0b8a97d4ba025b1f6171c
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2017-01-11 23:47:54 +0300

    Make x,y,z-constructor for vec3 inline

--- Utility/src/vec3.cpp
+++ Utility/src/vec3.cpp
@@ -5,8 +5,6 @@
 namespace VM {
 
 ///Constructors
-vec3::vec3(const float x, const float y, const float z): x(x), y(y), z(z) {}
-
 vec3::vec3(const float x): x(x), y(x), z(x) {}
 
 vec3::vec3(const float* coords): x(coords[0]), y(coords[1]), z(coords[2]) {}
