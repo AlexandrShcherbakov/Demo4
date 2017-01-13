bf0a4a013e21100a4e56858b51e856c39d6d9508
Alexandr Shcherbakov <alex.shcherbakov.as@gmail.com>
2017-01-14 01:17:58 +0300

    Remove mutual operators for vec3 and vector3

--- External/include/Utility/VM/vec3.h
+++ External/include/Utility/VM/vec3.h
@@ -11,70 +11,12 @@
 
 namespace VM {
 
-/*template<>
-class vector3<float> {
-public:
-	///Coordinates
-    float x, y, z;
-
-    ///Constructors
-    vector3<float>(): x(0), y(0), z(0) {}
-    vector3<float>(const float x, const float y, const float z): x(x), y(y), z(z) {}
-    vector3<float>(const float x): x(x), y(x), z(x) {}
-    vector3<float>(const float* coords): x(coords[0]), y(coords[1]), z(coords[2]) {}
-
-    ///Operators
-    inline friend vec3 operator+(const vec3& v, const vec3& w) {
-        return vec3(v.x + w.x, v.y + w.y, v.z + w.z);
-    }
-    inline friend vec3 operator-(const vec3& v, const vec3& w) {
-        return vec3(v.x - w.x, v.y - w.y, v.z - w.z);
-    }
-    inline friend vec3 operator*(const vec3& v, const vec3& w) {
-        return vec3(v.x * w.x, v.y * w.y, v.z * w.z);
-    }
-    inline friend vec3 operator/(const vec3& v, const vec3& w) {
-        return vec3(v.x / w.x, v.y / w.y, v.z / w.z);
-    }
-    vec3 operator-() const {
-        return vec3(-x, -y, -z);
-    }
-    inline friend vec3 operator+=(vec3& v, const vec3& w) {
-        return v = v + w;
-    }
-    inline friend vec3 operator-=(vec3& v, const vec3& w) {
-        return v = v - w;
-    }
-    inline friend vec3 operator*=(vec3& v, const vec3& w) {
-        return v = v * w;
-    }
-    inline friend vec3 operator/=(vec3& v, const vec3& w) {
-        return v = v / w;
-    }
-};*/
-
 bool operator==(const vec3 &v, const vec3& w);
-
-std::ostream& operator<<(std::ostream& out, const vec3& v);
-
-float min(const vec3& v);
-
-float max(const vec3& v);
-
-vec3 min(const vec3& v, const vec3& w);
-
-vec3 max(const vec3& v, const vec3& w);
 
 ///Length of vector for similarity to openGL syntax
 float length(const vec3 &v);
 
 vec3 normalize(const vec3 &v);
-
-///Dot production
-float dot(const vec3 &v, const vec3 &w);
-
-///Cross production
-vec3 cross(const vec3 &v, const vec3 &w);
 
 ///Cosine between two vectors
 float cos(const vec3 &v, const vec3 &w);
--- Utility/src/vec3.cpp
+++ Utility/src/vec3.cpp
@@ -8,27 +8,6 @@ bool operator==(const vec3 &v, const vec3& w) {
         && std::abs(v.z - w.z) < VEC_EPS;
 }
 
-std::ostream& operator<<(std::ostream& out, const vec3& v)
-{
-    return out << "(" << v.x << "; " << v.y << "; " << v.z << ")";
-}
-
-float min(const vec3& v) {
-    return std::min(std::min(v.x, v.y), v.z);
-}
-
-float max(const vec3& v) {
-	return std::max(std::max(v.x, v.y), v.z);
-}
-
-vec3 min(const vec3& v, const vec3& w) {
-    return vec3(std::min(v.x, w.x), std::min(v.y, w.y), std::min(v.z, w.z));
-}
-
-vec3 max(const vec3& v, const vec3& w) {
-    return vec3(std::max(v.x, w.x), std::max(v.y, w.y), std::max(v.z, w.z));
-}
-
 vec3 normalize(const vec3& v) {
     float len = length(v);
     if (len == 0.0f) throw "Null-length vector normalization.";
@@ -37,18 +16,6 @@ vec3 normalize(const vec3& v) {
 
 float length(const vec3 &v) {
     return sqrt(dot(v, v));
-}
-
-///Dot production
-float dot(const vec3 &v, const vec3 &w) {
-    return v.x * w.x + v.y * w.y + v.z * w.z;
-}
-
-///Cross production
-vec3 cross(const vec3 &v, const vec3 &w) {
-    return vec3(v[1] * w[2] - v[2] * w[1],
-                v[2] * w[0] - v[0] * w[2],
-                v[0] * w[1] - v[1] * w[0]);
 }
 
 ///Cosine between two vectors
