89f1ba73e7c399c2e1bbd62169440f9b5b776c5a
Alexander Shcherbakov <19a_shc@lab.graphicon.ru>
2017-02-09 08:26:16 +0300

    Add utils for dds image creating and checking

---
+++ FFDiff/main.cpp
@@ -0,0 +1,78 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "Utility.h"
+
+using namespace std;
+
+void InitializeGLUT(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitContextVersion(4, 3);
+}
+
+const string GenPath(const string& name, const int size, const bool image=false) {
+    stringstream ss;
+    string suffix = image ? ".dds" : ".bin";
+    ss << "../Scenes/colored-sponza/" << name << size << suffix;
+    return ss.str();
+}
+
+const vector<VM::vec3> ReadFF(const int size) {
+    ifstream in(GenPath("FF", size), ios::binary | ios::in);
+    int ffSize;
+    in.read((char*)&ffSize, sizeof(ffSize));
+    vector<VM::vec3> data(ffSize * ffSize);
+    for (int i = 0; i < ffSize; ++i) {
+        for (int j = 0; j < ffSize; ++j) {
+            int index = i * ffSize + j;
+            in.read((char*)&data[index], sizeof(data[index]));
+        }
+    }
+    return data;
+}
+
+const vector<VM::vec3> ReadTexture(const int size) {
+    int width, height, channels;
+    auto rawData = SOIL_load_image(GenPath("FF", size, true).c_str(), &width, &height, &channels, 0);
+
+    VM::vec3 minValue, maxValue;
+    ifstream in(GenPath("AdditionalInfo", size), ios::binary | ios::in);
+    in.read((char*)&minValue, sizeof(minValue));
+    in.read((char*)&maxValue, sizeof(maxValue));
+
+    vector<VM::vec3> ff;
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            VM::vec3 value;
+            for (int h = 0; h < channels; ++h) {
+                value[h] = rawData[(i * width + j) * channels];
+            }
+            value = (value / 255) * maxValue + minValue;
+            ff.push_back(value);
+        }
+    }
+    return ff;
+}
+
+void Compare(const vector<VM::vec3>& ff1, const vector<VM::vec3>& ff2) {
+    cout << ff1.size() << ' ' << ff2.size() << endl;
+    float result = 0;
+    for (uint i = 0; i < ff1.size(); ++i) {
+        result += length(ff1[i] - ff2[i]);
+    }
+    cout << result << endl;
+}
+
+int main(int argc, char** argv) {
+    InitializeGLUT(argc, argv);
+    int size = 20;
+    if (argc > 1) {
+        istringstream iss(argv[1]);
+        iss >> size;
+        cout << "Current size: " << size << endl;
+    }
+    Compare(ReadFF(size), ReadTexture(size));
+}
---
+++ Matrix2Image/main.cpp
@@ -0,0 +1,76 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "Utility.h"
+
+using namespace std;
+
+const string GenPath(const string& name, const int size, const bool image=false) {
+    stringstream ss;
+    string suffix = image ? ".dds" : ".bin";
+    ss << "../Scenes/colored-sponza/" << name << size << suffix;
+    return ss.str();
+}
+
+const vector<VM::vec3> ReadFF(const int size) {
+    ifstream in(GenPath("FF", size), ios::binary | ios::in);
+    int ffSize;
+    in.read((char*)&ffSize, sizeof(ffSize));
+    vector<VM::vec3> data(ffSize * ffSize);
+    for (int i = 0; i < ffSize; ++i) {
+        for (int j = 0; j < ffSize; ++j) {
+            int index = i * ffSize + j;
+            in.read((char*)&data[index], sizeof(data[index]));
+        }
+    }
+    return data;
+}
+
+typedef unsigned char byte;
+
+const vector<byte> PrepareColors(const vector<VM::vec3>& data, const int size) {
+    VM::vec3 minValue(1.0f / VEC_EPS);
+    VM::vec3 maxValue(-1.0f / VEC_EPS);
+    for(auto& value: data) {
+        minValue = VM::min(minValue, value);
+        maxValue = VM::max(maxValue, value);
+    }
+    const int components = VM::vec3::Components;
+    vector<byte> colors(data.size() * components);
+    for (uint i = 0; i < data.size(); ++i) {
+        VM::vec3 color = (data[i] - minValue) / maxValue * 255;
+        for (int j = 0; j < components; ++j) {
+            colors[i * components + j] = round(color[j]);
+        }
+    }
+    ofstream out(GenPath("AdditionalInfo", size), ios::out | ios::binary);
+    out.write((char*)&minValue, sizeof(minValue));
+    out.write((char*)&maxValue, sizeof(maxValue));
+    return colors;
+}
+
+void SaveImage(const vector<byte>& data, const int size) {
+    const int channels = VM::vec3::Components;
+    const int side = static_cast<int>(sqrt(data.size() / channels));
+    SOIL_save_image(GenPath("FF", size, true).c_str(), SOIL_SAVE_TYPE_DDS, side, side, channels, data.data());
+}
+
+void InitializeGLUT(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitContextVersion(4, 3);
+}
+
+int main(int argc, char** argv) {
+    InitializeGLUT(argc, argv);
+    int size = 20;
+    if (argc > 1) {
+        istringstream iss(argv[1]);
+        iss >> size;
+        cout << "Current size: " << size << endl;
+    }
+    SaveImage(PrepareColors(ReadFF(size), size), size);
+}
